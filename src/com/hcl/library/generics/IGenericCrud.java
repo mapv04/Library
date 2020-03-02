3b80be308e1f578a6219dd774b301edc4a92fb6e
JesusRRr <rolonjesus1996@gmail.com>
2020-03-02 14:45:32 -0600

    feature: implements methods in GenericCrudImpl

--- src/com/hcl/library/generics/IGenericCrud.java
+++ src/com/hcl/library/generics/IGenericCrud.java
@@ -1,13 +1,15 @@
 package com.hcl.library.generics;
 
+import java.util.function.Predicate;
+
 public interface IGenericCrud<T> {
 
 	boolean create(T entity);
 
 	boolean deleteById(int id);
 
-	boolean update(int id);
+	boolean update(int id, T newData);
 
-	T findById(int id);
+	//T findBy(Predicate<T> search);
 
 }
