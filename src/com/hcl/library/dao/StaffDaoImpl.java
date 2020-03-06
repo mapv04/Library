8f105abd3516b816816d6285cb9d02262e3ed3f4
JesusRRr <rolonjesus1996@gmail.com>
2020-03-06 11:08:28 -0600

    feature: create package model.dao and move the bussiness classes

--- src/com/hcl/library/dao/StaffDaoImpl.java
+++ src/com/hcl/library/dao/StaffDaoImpl.java
@@ -1,7 +1,7 @@
 package com.hcl.library.dao;
 
 import com.hcl.library.generics.GenericCrudImpl;
-import com.hcl.library.model.Staff;
+import com.hcl.library.model.bo.Staff;
 
 public class StaffDaoImpl extends GenericCrudImpl<Staff>{
 
