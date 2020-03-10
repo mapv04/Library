3889740786c59d6ebb60fc08dc7cb73cbbdc09d5
Miguel <miguelpartida080497@gmail.com>
2020-03-10 11:43:11 -0600

    fix: database user

--- script.sql
+++ script.sql
@@ -97,5 +97,5 @@ CREATE TABLE loan_book(
 );
 
 CREATE USER IF NOT EXISTS 'user_library'@'localhost' IDENTIFIED BY '1234';
-GRANT ALL PRIVILEGES ON library TO 'user_library'@'locaLhost';
+GRANT ALL PRIVILEGES ON library.* TO 'user_library'@'localhost';
 FLUSH PRIVILEGES;
