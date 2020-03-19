9a1442896e867bb5d9eb2bca0cfc007dd491e819
Diana Siqueiros <DSV@ARS.local>
2020-03-19 15:44:39 -0600

    feature: add customer map

--- src/com/hcl/library/model/utils/TransformDataUtils.java
+++ src/com/hcl/library/model/utils/TransformDataUtils.java
@@ -1,6 +1,8 @@
 package com.hcl.library.model.utils;
 
+import com.hcl.library.model.bo.CustomerBO;
 import com.hcl.library.model.bo.LoanBO;
+import com.hcl.library.model.po.CustomerPO;
 import com.hcl.library.model.po.LoanPO;
 
 public final class TransformDataUtils {
@@ -12,5 +14,10 @@ public final class TransformDataUtils {
 	public static final LoanPO map(LoanBO businessLoan) {
 		return null;
 	}
-	
+	public static final CustomerBO map(CustomerPO persistenceCustomer) {
+		return null;
+	}
+	public static final CustomerPO map(CustomerBO busnessCustomer) {
+		return null;
+	}
 }
