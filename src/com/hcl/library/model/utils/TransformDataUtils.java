5edb964160b29616e407bcacc38b8146b8560130
Miguel <miguelpartida080497@gmail.com>
2020-03-19 14:07:46 -0600

    utility class to transform po an bo

---
+++ src/com/hcl/library/model/utils/TransformDataUtils.java
@@ -0,0 +1,16 @@
+package com.hcl.library.model.utils;
+
+import com.hcl.library.model.bo.LoanBO;
+import com.hcl.library.model.po.LoanPO;
+
+public final class TransformDataUtils {
+
+	public static final LoanBO map(LoanPO persistenceLoan) {
+		return null;
+	}
+	
+	public static final LoanPO map(LoanBO businessLoan) {
+		return null;
+	}
+	
+}
