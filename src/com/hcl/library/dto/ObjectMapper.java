237befe68ea6c016a4da17715ac4fbe6850fee5e
JesusRRr <rolonjesus1996@gmail.com>
2020-03-20 13:38:39 -0600

    feature : create BookDto class

--- src/com/hcl/library/dto/ObjectMapper.java
+++ src/com/hcl/library/dto/ObjectMapper.java
@@ -46,38 +46,6 @@ public final class ObjectMapper {
 		return customer;
 	}
 	
-	public static final BookBO map(BookPO persistenceBook) {
-		BookBO book = new BookBO();
-		book.setId(persistenceBook.getId());
-		book.setName(persistenceBook.getName());
-		book.setIsbn(persistenceBook.getIsbn());
-		book.setEdition(persistenceBook.getEdition());
-		book.setEditorial(persistenceBook.getEditorial());
-		book.setCategory(persistenceBook.getCategory());
-		book.setLanguage(persistenceBook.getLanguage());
-		book.setBookcover(persistenceBook.getBookcover());
-		book.setStatus(persistenceBook.getStatus());
-		//book.setAuthors(persistenceBook.getAuthors());
-		//book.setLoans(persistenceBook.getLoans());
-		
-		return book;
-	}
 	
-	public static final BookPO map(BookBO businessBook) {
-		BookPO book = new BookPO();
-		book.setId(businessBook.getId());
-		book.setName(businessBook.getName());
-		book.setEdition(businessBook.getEdition());
-		book.setEditorial(businessBook.getEdition());
-		book.setIsbn(businessBook.getIsbn());
-		book.setCategory(businessBook.getCategory());
-		book.setLanguage(businessBook.getLanguage());
-		book.setBookcover(businessBook.getBookcover());
-		book.setStatus(businessBook.getStatus());
-		//book.setAuthors(businessBook.getAuthors());
-		//book.setLoans(businessBook.getLoans());
-		
-		return book;
-	}
 	
 }
