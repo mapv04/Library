d0290290a968780825482660c215c670427933d3
JesusRRr <rolonjesus1996@gmail.com>
2020-03-20 14:49:46 -0600

    feature: staffDto created

---
+++ src/com/hcl/library/dto/StaffDto.java
@@ -0,0 +1,37 @@
+package com.hcl.library.dto;
+
+import com.hcl.library.model.bo.StaffBO;
+import com.hcl.library.model.po.StaffPO;
+
+public class StaffDto {
+	public static final StaffBO map(StaffPO persistenceStaff) {
+		StaffBO staff = new StaffBO();
+		staff.setId(persistenceStaff.getId());
+		staff.setName(persistenceStaff.getName());
+		staff.setLastName(persistenceStaff.getLastName());
+		staff.setCurp(persistenceStaff.getCurp());
+		staff.setUserName(persistenceStaff.getUserName());
+		staff.setPassword(persistenceStaff.getPassword());
+		/* TODO
+		 staff.setLoan(persistenceStaff.getLoan());
+		 */
+		
+		return staff;
+	}
+	public static final StaffPO map(StaffBO businessStaff) {
+		StaffPO staff = new StaffPO();
+		staff.setId(businessStaff.getId());
+		staff.setName(businessStaff.getName());
+		staff.setLastName(businessStaff.getLastName());
+		staff.setCurp(businessStaff.getCurp());
+		staff.setUserName(businessStaff.getUserName());
+		staff.setPassword(businessStaff.getPassword());
+		
+		/* TODO
+		 staff.setLoan(persistenceStaff.getLoan());
+		 */
+		
+		return staff;
+	}	
+	
+}
