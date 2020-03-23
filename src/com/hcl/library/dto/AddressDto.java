48e1b4f540bcca1a83aabf1a48c5d50e64b98cb1
Diana Siqueiros <DSV@ARS.local>
2020-03-23 12:37:08 -0600

    feature: add set and get address in customer class

--- src/com/hcl/library/dto/AddressDto.java
+++ src/com/hcl/library/dto/AddressDto.java
@@ -7,23 +7,27 @@ public class AddressDto {
 	
 	public static final AddressBO map(AddressPO persistanceAddress) {
 		AddressBO address = new AddressBO();
+		address.setId(persistanceAddress.getId());
 		address.setStreet(persistanceAddress.getStreet());
 		address.setNumber(persistanceAddress.getNumber());
 		address.setPostalCode(persistanceAddress.getNumber());
 		address.setCity(persistanceAddress.getCity());
 		address.setState(persistanceAddress.getState());
 		address.setCountry(persistanceAddress.getCountry());
+		
 		return address;
 	}
 
 	public static final AddressPO map(AddressBO businessAddress) {
 		AddressPO address = new AddressPO();
+		address.setId(businessAddress.getId());
 		address.setStreet(businessAddress.getStreet());
 		address.setNumber(businessAddress.getNumber());
 		address.setPostalCode(businessAddress.getPostalCode());
 		address.setCity(businessAddress.getCity());
 		address.setState(businessAddress.getState());
 		address.setCountry(businessAddress.getCountry());
+		
 		return address;
 	}
 }
