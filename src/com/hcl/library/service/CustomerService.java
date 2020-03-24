57c82bef435e5d6f938381824bef1e75232f3863
Diana Siqueiros <DSV@ARS.local>
2020-03-24 13:09:40 -0600

    fix: change the data access to the method getInstance

--- src/com/hcl/library/service/CustomerService.java
+++ src/com/hcl/library/service/CustomerService.java
@@ -9,13 +9,13 @@ import com.hcl.library.model.po.CustomerPO;
 
 public class CustomerService {
 	private static CustomerDao customerDao;
-	public static CustomerService instance;
+	private static CustomerService instance;
 	
 	private CustomerService() {
 		customerDao = new CustomerDao();
 	}
 	
-	private static CustomerService getInstance() {
+	public static CustomerService getInstance() {
 		if(instance == null) {
 			instance = new CustomerService();
 		}
